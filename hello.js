90a94bf702c934c24780dce73aa71d5a48d16dee
james <jtc5311@kookmin.ac.kr>
2020-11-17 14:52:41 +0900

    add js

---
+++ hello.js
@@ -0,0 +1,7 @@
+function doSomething(){
+    var name = document.getElementById("user_name").value;
+
+    document.getElementById("hello_to_somebody").innerHTML = 
+        "hello." + name +  "!!" 
+
+}
